37ebfd01557f102c738f539f3744d3a375fbc9f5
kevinlac <kevinlac@tuta.io>
2026-07-11 17:58:07 +0930

    compiling submissions

--- worker/cmd/server/main.go
+++ worker/cmd/server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
 	"worker/internal/config"
 	"worker/internal/db"
+	"worker/internal/judge"
 )
 
 func main() {
@@ -18,6 +20,19 @@ func main() {
 	defer rawDB.Close()
 
 	fmt.Printf("Connected to db successfully.\n")
+
+	store := db.NewStore(rawDB)
+
+	// should only return our one submission from example data
+	allSubmissions, err := store.ListSubmissionsByProblem(context.Background(), "sum-two-numbers")
+	fmt.Printf("No. of submissions for sum-two-numbers: %d\n", len(allSubmissions))
+
+	err = judge.Compile(context.Background(), cfg, allSubmissions[0])
+	if err != nil {
+		fmt.Printf("Error compiling!: %v\n", err)
+	} else {
+		fmt.Printf("Compiled submission successfully.\n") 
+	}
 
 	for {
 		time.Sleep(1 * time.Second)
